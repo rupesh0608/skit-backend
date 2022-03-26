cbc6f4ad4d8da0a376089ddbd1a26dcd150ca122
Rupesh Kumar deshmukh <rupeshdeshmukh618@gmail.com>
2022-03-26 14:23:42 +0530

    initial commit

---
+++ src/main/kotlin/com/rdktechnologies/skit/SkitApplication.kt
@@ -0,0 +1,11 @@
+package com.rdktechnologies.skit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkitApplication
+
+fun main(args: Array<String>) {
+	runApplication<SkitApplication>(*args)
+}
